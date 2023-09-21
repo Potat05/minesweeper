d94594536aaa1f31d35f578384d257e3dd8e27c6
Potat05 <williamkoerber6@gmail.com>
2023-09-21 00:12:47 -0500

    refactor segment display

--- src/components/SegmentDisplay.svelte
+++ src/components/SegmentDisplay.svelte
@@ -4,20 +4,22 @@
     export let maxDigits: number = 3;
     export let number: number;
 
-    $: digits = Math.floor(number).toString().padStart(minDigits, '0').split('').slice(-maxDigits).map(Number);
+    $: digits = Math.floor(number).toString().padStart(minDigits, ' ').slice(-maxDigits).split('');
 
-    const DIGIT_SEGMENT_MAP: [ boolean, boolean, boolean, boolean, boolean, boolean, boolean ][] = [
-        [ true, true, true, true, true, true, false ],
-        [ false, true, true, false, false, false, false ],
-        [ true, true, false, true, true, false, true ],
-        [ true, true, true, true, false, false, true ],
-        [ false, true, true, false, false, true, true ],
-        [ true, false, true, true, false, true, true ],
-        [ true, false, true, true, true, true, true ],
-        [ true, true, true, false, false, false, false ],
-        [ true, true, true, true, true, true, true ],
-        [ true, true, true, true, false, true, true ]
-    ];
+    const CHARMAP: {[key: string]: [ boolean, boolean, boolean, boolean, boolean, boolean, boolean ]} = {
+        '0': [ true, true, true, true, true, true, false ],
+        '1': [ false, true, true, false, false, false, false ],
+        '2': [ true, true, false, true, true, false, true ],
+        '3': [ true, true, true, true, false, false, true ],
+        '4': [ false, true, true, false, false, true, true ],
+        '5': [ true, false, true, true, false, true, true ],
+        '6': [ true, false, true, true, true, true, true ],
+        '7': [ true, true, true, false, false, false, false ],
+        '8': [ true, true, true, true, true, true, true ],
+        '9': [ true, true, true, true, false, true, true ],
+        '-': [ false, false, false, false, false, false, true ],
+        ' ': [ false, false, false, false, false, false, false]
+    }
 
 </script>
 
@@ -54,13 +56,13 @@
                 <line stroke="#800" stroke-width="1px" y2="1"/>
         </pattern>
         </defs>
-        <path class:on={DIGIT_SEGMENT_MAP[digit][0]} d="M1 1l1-1h6l1 1-1 1H2z"/>
-        <path class:on={DIGIT_SEGMENT_MAP[digit][1]} d="M9 1l1 1v6L9 9 8 8V2z"/>
-        <path class:on={DIGIT_SEGMENT_MAP[digit][2]} d="M9 9l1 1v6l-1 1-1-1v-6z"/>
-        <path class:on={DIGIT_SEGMENT_MAP[digit][3]} d="M9 17l-1 1H2l-1-1 1-1h6z"/>
-        <path class:on={DIGIT_SEGMENT_MAP[digit][4]} d="M1 17l-1-1v-6l1-1 1 1v6z"/>
-        <path class:on={DIGIT_SEGMENT_MAP[digit][5]} d="M1 9L0 8V2l1-1 1 1v6z"/>
-        <path class:on={DIGIT_SEGMENT_MAP[digit][6]} d="M1 9l1-1h6l1 1-1 1H2z"/>
+        <path class:on={CHARMAP[digit][0]} d="M1 1l1-1h6l1 1-1 1H2z"/>
+        <path class:on={CHARMAP[digit][1]} d="M9 1l1 1v6L9 9 8 8V2z"/>
+        <path class:on={CHARMAP[digit][2]} d="M9 9l1 1v6l-1 1-1-1v-6z"/>
+        <path class:on={CHARMAP[digit][3]} d="M9 17l-1 1H2l-1-1 1-1h6z"/>
+        <path class:on={CHARMAP[digit][4]} d="M1 17l-1-1v-6l1-1 1 1v6z"/>
+        <path class:on={CHARMAP[digit][5]} d="M1 9L0 8V2l1-1 1 1v6z"/>
+        <path class:on={CHARMAP[digit][6]} d="M1 9l1-1h6l1 1-1 1H2z"/>
     </svg>
 
 {/each}
