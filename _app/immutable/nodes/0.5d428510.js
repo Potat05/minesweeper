9bcef9b8f98d0691c7001e10a0b31414bea2aa12
Potat05 <Potat05@users.noreply.github.com>
2023-09-22 01:13:26 +0000

    deploy: 271a3adb8a3f809b86b47e137dc748f2ae65c332

---
+++ _app/immutable/nodes/0.5d428510.js
@@ -0,0 +1,35 @@
+import{s as c,c as m,u,g as d,d as h}from"../chunks/scheduler.bc760b9c.js";import{S as _,i as p,g,s as y,x as v,h as w,y as $,f,c as x,z as C,a as S,d as b,t as P}from"../chunks/index.b6611d32.js";function k(a){let n,l=`body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            width: 100vw;
+            height: 100vh;
+
+            margin: 0px;
+
+            background-color: #C6C6C6;
+        }
+
+        @font-face {
+            font-family: 'Alagard';
+            font-style: normal;
+            font-weight: 400;
+            src: url('/minesweeper/fonts/alagard.ttf') format('truetype');
+
+            
+            font-smooth: never;
+            -webkit-font-smoothing: none;
+            text-rendering: geometricPrecision;
+        }
+
+        @font-face {
+            font-family: 'Pixel Sans Serif';
+            font-style: normal;
+            font-weight: 400;
+            src: url('/minesweeper/fonts/Pixel Sans Serif.ttf') format('truetype');
+
+            font-smooth: never;
+            -webkit-font-smoothing: none;
+            text-rendering: geometricPrecision;
+        }`,i,s;const r=a[1].default,t=m(r,a,a[0],null);return{c(){n=g("style"),n.textContent=l,i=y(),t&&t.c(),this.h()},l(e){const o=v("svelte-1iss6eg",document.head);n=w(o,"STYLE",{"data-svelte-h":!0}),$(n)!=="svelte-15qsq9y"&&(n.textContent=l),o.forEach(f),i=x(e),t&&t.l(e),this.h()},h(){document.title="Minesweeper"},m(e,o){C(document.head,n),S(e,i,o),t&&t.m(e,o),s=!0},p(e,[o]){t&&t.p&&(!s||o&1)&&u(t,r,e,e[0],s?h(r,e[0],o,null):d(e[0]),null)},i(e){s||(b(t,e),s=!0)},o(e){P(t,e),s=!1},d(e){e&&f(i),f(n),t&&t.d(e)}}}function q(a,n,l){let{$$slots:i={},$$scope:s}=n;return a.$$set=r=>{"$$scope"in r&&l(0,s=r.$$scope)},[s,i]}class j extends _{constructor(n){super(),p(this,n,q,k,c,{})}}export{j as component};
